cf8781f0b19aa544d6f61826a8016f64522ce722
Connor Holyday <connorholyday@gmail.com>
2014-01-02 19:05:50 +0000

    Correct file links + H5BP setup

--- application/view/master/footer.php
+++ application/view/master/footer.php
@@ -1,3 +1,5 @@
-
-</body>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="bower_components/jquery/jquery.js"><\/script>')</script>
+		<script src="/assets/scripts/production.js"></script>
+	</body>
 </html>
--- application/view/master/header.php
+++ application/view/master/header.php
@@ -1,7 +1,40 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<?php flush(); ?>
-<body>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Sell My Gadger's</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Favicons -->
+
+		<link rel="stylesheet" href="bower_components/normalize-css/normalize.css" />
+
+		<link rel="stylesheet" href="/assets/css/global.css" />
+
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="/assets/js/libs/selectivizr-min.js"></script>
+		<![endif]-->
+
+		<!-- Google Analytics <script type="text/javascript">
+
+			var _gaq = _gaq || [];
+			var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+			_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+			_gaq.push(['_setAccount', 'UA-********-2']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+
+		</script> -->
+
+		<script type="text/javascript" src="/assets/scripts/vendor/modernizr.js"></script>
+
+	</head>
+	<?php flush(); ?>
+	<body>
