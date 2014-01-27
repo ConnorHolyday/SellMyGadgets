10956607428f34442230fbf77755abc35259fe59
Connor Holyday <connorholyday@gmail.com>
2014-01-27 09:26:36 +0000

    Added new directory structure to footer links

--- application/view/master/footer.php
+++ application/view/master/footer.php
@@ -1,5 +1,5 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="bower_components/jquery/jquery.js"><\/script>')</script>
-		<script src="/assets/scripts/production.js"></script>
+		<script src="/assets/js/main.js"></script>
 	</body>
 </html>
