7fd309d0eb1524d52e50cdeae9e5d1447824cd2e
Connor Holyday <connorholyday@gmail.com>
2014-01-27 09:22:44 +0000

    Updated grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,93 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            // 2. Configuration for concatinating files goes here.
-            dist: {
-                src: [
-                    //'bower_components/jquery/jquery.js', // REMOVE for production
-                    'assets/scripts/main.js'  // This specific file
-                ],
-                dest: 'assets/scripts/production.js',
-            }
-        },
-        uglify: {
-            build: {
-                src: 'assets/scripts/production.js',
-                dest: 'assets/scripts/production.js'
-            }
-        },
         imagemin: {
             dynamic: {
                 files: [{
                     expand: true,
                     cwd: 'assets/img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'assets/img/'
+                    src: ['*.{png,jpg,gif}', '**/*.{png,jpg,gif}']
                 }]
             }
         },
+
+
         sass: {
-            dist: {
+            styles: {
                 options: {
-                    style: 'compressed'
+                    style: 'expanded',
+                    sourcemap: true
                 },
                 files: {
-                    'assets/scss/global.scss': 'assets/scss/partials/*.scss',
-                    'assets/css/global.css': 'assets/scss/global.scss'
+                    'assets/css/styles.css': 'assets/scss/styles.scss'
                 }
             }
         },
         autoprefixer: {
             options: {
-                browsers: ['last 2 versions']
+                browsers: ['last 2 versions', 'ie 8', 'ie 9']
             },
             single_file: {
-                 src: 'assets/css/styles.css',
-               },
+                src: 'assets/css/styles.css'
+            },
         },
+        cmq: {
+            options: {
+                log: false
+            },
+            styles: {
+                files: {
+                    'assets/css/styles.css' : 'assets/css/styles.css'
+                }
+            }
+        },
+        csslint: {
+            options: {
+                import: false,
+                csslintrc: '.csslintrc'
+            },
+            lint: {
+                src: ['assets/css/styles.css']
+            }
+        },
+        concat: {
+            css: {
+                src: [
+                    'assets/css/styles.css'
+                ],
+                dest: 'assets/css/styles.css',
+            }
+        },
+        cssmin: {
+            minify: {
+                files: {
+                    'assets/css/styles.min.css' : 'assets/css/styles.css'
+                }
+            }
+        },
+
+
         jshint: {
             options: {
                 reporter: require('jshint-stylish')
             },
-            target: ['assets/scripts/main.js']
+            target: ['assets/js/main.js']
+        },
+        uglify: {
+            build: {
+                src: 'assets/js/main.js',
+                dest: 'assets/js/main.min.js'
+            }
         },
         modernizr: {
 
             // [REQUIRED] Path to the build you're using for development.
-            "devFile" : "assets/scripts/vendor/modernizr.js",
+            "devFile" : "assets/js/libs/modernizr.js",
 
             // [REQUIRED] Path to save out the built file.
-            "outputFile" : "assets/scripts/vendor/modernizr.js",
+            "outputFile" : "assets/js/libs/modernizr.js",
 
             // Based on default settings on http://modernizr.com/download/
             "extra" : {
@@ -96,7 +122,7 @@ module.exports = function(grunt) {
 
             // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
             // You can override this by defining a "files" array below.
-            "files" : [ 'assets/scripts/production.js', 'assets/css/global.css' ],
+            "files" : [ 'assets/js/main.js', 'assets/css/styles.css' ],
 
             // When parseFiles = true, matchCommunityTests = true will attempt to
             // match user-contributed tests.
@@ -105,39 +131,62 @@ module.exports = function(grunt) {
             // Have custom Modernizr tests? Add paths to their location here.
             "customTests" : []
         },
+
+
         watch: {
-            options: {
-                livereload: true,
+            html: {
+                files: ['index.html'],
+                tasks: ['htmlhint'],
+                options: {
+                    spawn: false,
+                },
+            },
+            images: {
+                files: ['assets/img/*.{png,jpg,gif}', 'assets/img/**/*.{png,jpg,gif}'],
+                tasks: ['newer:imagemin'],
+                options: {
+                    spawn: false,
+                },
             },
             scripts: {
-                files: ['assets/scripts/*.js'],
-                tasks: ['concat', 'jshint', 'uglify'],
+                files: ['assets/js/*.js'],
+                tasks: ['modernizr', 'newer:jshint', 'uglify'],
                 options: {
                     spawn: false,
                 },
             },
             css: {
-                files: ['assets/scss/partials/*.scss'],
-                tasks: ['sass', 'autoprefixer'],
+                files: ['assets/scss/*.scss', 'assets/scss/**/*.scss'],
+                tasks: ['modernizr', 'sass:styles', 'newer:autoprefixer', 'cmq', 'csslint', 'concat:css', 'cssmin'],
                 options: {
                     spawn: false,
                 }
+            },
+            livereload: {
+                options: {
+                    livereload: true,
+                },
+                files: ['*.html', '*.php', 'js/**/*.{js,json}', 'assets/css/*.css', 'assets/img/**/*.{png,jpg,jpeg,gif,webp,svg}', 'assets/img/*.{png,jpg,jpeg,gif,webp,svg}'],
             }
         }
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks("grunt-modernizr");
+    grunt.loadNpmTasks("grunt-newer");
+    grunt.loadNpmTasks('grunt-combine-media-queries');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'sass', 'jshint', 'autoprefixer', 'modernizr']);
+    grunt.registerTask('default', ['imagemin', 'modernizr', 'jshint', 'uglify', 'sass', 'autoprefixer', 'cmq', 'csslint', 'concat:css', 'cssmin']);
+    grunt.registerTask('styles', ['sass', 'autoprefixer', 'cmq', 'csslint', 'concat:css', 'cssmin']);
+    grunt.registerTask('scripts', ['modernizr', 'uglify', 'jshint']);
     grunt.registerTask('auto', ['watch']);
 };
