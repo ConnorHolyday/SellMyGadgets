b47c478f214635d6608d45eb27b15c76a0149e41
Connor Holyday <connorholyday@gmail.com>
2014-02-13 22:02:42 +0000

    Started breadcrumb

--- application/view/Product/all.php
+++ application/view/Product/all.php
@@ -1,7 +1,15 @@
-<h1>Products Page - all</h1>
-
 <?php
 foreach ($this->products as $product) {
 		echo $product;
 }
 ?>
+
+<div class="wrapper">
+	<h1>Products Page - all</h1>
+
+	<nav class="breadcrumb breadcrumb__product">
+		<a href="/">Home</a>
+		<a href="#">Product Search</a>
+		<a href="#">Product Search for "iPhone"</a>
+	</nav>
+</div>
