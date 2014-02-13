f53d742f095a074fdd32a170fe7d2c49599ef54a
Connor Holyday <connorholyday@gmail.com>
2014-02-13 21:58:55 +0000

    Implemented grid to footer

--- application/view/master/footer.php
+++ application/view/master/footer.php
@@ -1,10 +1,15 @@
 <footer class="footer__main bg--dark cf">
 	<div class="wrapper">
-		<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
 
-		<p>Quas eligendi totam sapiente adipisci officia rem vero quidem distinctio. Totam, assumenda, vel perferendis amet incidunt nihil ea quos fugiat laborum magni.</p>
+	<div class="row">
+		<div class="col m-all t-2 d-5">
+			<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
 
-		<p>Quas eligendi totam sapiente adipisci officia rem vero quidem distinctio. Totam, assumenda, vel perferendis amet incidunt nihil ea quos fugiat laborum magni.</p>
+			<p>Quas eligendi totam sapiente adipisci officia rem vero quidem distinctio. Totam, assumenda, vel perferendis amet incidunt nihil ea quos fugiat laborum magni.</p>
+
+			<p>Quas eligendi totam sapiente adipisci officia rem vero quidem distinctio. Totam, assumenda, vel perferendis amet incidunt nihil ea quos fugiat laborum magni.</p>
+		</div>
+	</div>
 
 		<div class="sub-footer cf">
 			<ul class="pull-left nav__footer inline-links inline-links--bar">
