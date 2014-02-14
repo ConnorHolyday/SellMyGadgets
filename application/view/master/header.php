eb2d83835b558d05000c22a503bdfee4857f6ca9
Connor Holyday <connorholyday@gmail.com>
2014-02-14 11:07:49 +0000

    Fixed favicon href

--- application/view/master/header.php
+++ application/view/master/header.php
@@ -9,7 +9,7 @@
 
 		<!-- Favicons -->
 
-		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="shortcut icon" href="/favicon.ico">
 
 		<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400' rel='stylesheet' type='text/css'>
 
