20e0f93be75f130ee12fb44db2f6a352b12e657e
Connor Holyday <connorholyday@gmail.com>
2014-02-28 14:33:28 +0000

    Added some links on the homepage

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -1,88 +1,86 @@
+<section class="wrapper row list-block">
+	<h1>Alternatively, you could browse our categories.</h1>
 
+	<div class="col d-2 list-block__wrap module--border">
+		<h2 class="list-block__title">Mobile</h2>
+		<ul class="list-block__list">
+			<li class="icon-uniE604">
+				<a href="#">Samsung</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Apple</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Nokia</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Blackberry</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">View More...</a>
+			</li>
+		</ul>
+	</div>
 
-	<section class="wrapper row list-block">
-		<h1>Alternatively, you could browse our categories.</h1>
+	<div class="col d-1">&nbsp;</div>
 
-		<div class="col d-2 list-block__wrap module--border">
-			<h2 class="list-block__title">Mobile</h2>
-			<ul class="list-block__list">
-				<li class="icon-uniE604">
-					<a href="#">Samsung</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Apple</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Nokia</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Blackberry</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">View More...</a>
-				</li>
-			</ul>
-		</div>
+	<div class="col d-2 list-block__wrap module--border">
+		<h2 class="list-block__title">Computer</h2>
+		<ul class="list-block__list">
+			<li class="icon-uniE604">
+				<a href="#">Samsung</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Apple</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Nokia</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Blackberry</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">View More...</a>
+			</li>
+		</ul>
+	</div>
 
-		<div class="col d-1">&nbsp;</div>
+	<div class="col d-1">&nbsp;</div>
 
-		<div class="col d-2 list-block__wrap module--border">
-			<h2 class="list-block__title">Computer</h2>
-			<ul class="list-block__list">
-				<li class="icon-uniE604">
-					<a href="#">Samsung</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Apple</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Nokia</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Blackberry</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">View More...</a>
-				</li>
-			</ul>
-		</div>
+	<div class="col d-2 list-block__wrap module--border">
+		<h2 class="list-block__title">Tablet</h2>
+		<ul class="list-block__list">
+			<li class="icon-uniE604">
+				<a href="#">Samsung</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Apple</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Nokia</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">Blackberry</a>
+			</li>
+			<li class="icon-uniE604">
+				<a href="#">View More...</a>
+			</li>
+		</ul>
+	</div>
 
-		<div class="col d-1">&nbsp;</div>
+	<a href="/product/all/" class="pull-right / btn btn--dark list-block__button">View all Categories</a>
+</section>
 
-		<div class="col d-2 list-block__wrap module--border">
-			<h2 class="list-block__title">Tablet</h2>
-			<ul class="list-block__list">
-				<li class="icon-uniE604">
-					<a href="#">Samsung</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Apple</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Nokia</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">Blackberry</a>
-				</li>
-				<li class="icon-uniE604">
-					<a href="#">View More...</a>
-				</li>
-			</ul>
-		</div>
+<section class="cta cta--sell bg--dark center">
+	<div class="wrapper">
+		<h1 class="left block">Maybe you have something that you're needing to sell? Click the button below to start the selling process.</h1>
+		<a href="/sell" class="btn btn--light btn--large inline-block">Sell My Gadgets</a>
+	</div>
+</section>
 
-		<a href="/product/all/" class="pull-right / btn btn--dark list-block__button">View all Categories</a>
-	</section>
-
-	<section class="cta cta--sell bg--dark center">
-		<div class="wrapper">
-			<h1 class="left block">Maybe you have something that you're needing to sell? Click the button below to start the selling process.</h1>
-			<a href="#" class="btn btn--light btn--large inline-block">Sell My Gadgets</a>
-		</div>
-	</section>
-
-	<section class="wrapper cta social--strip center">
-		<h1 class="left">Don't forget to check us out on these social networks and tell your friends about your new gadgets.</h1>
-		<a href="#" title="Facebook Link" class="icon icon--facebook"></a>
-		<a href="#" title="Google Plus Link" class="icon icon--google-plus"></a>
-		<a href="#" title="Twitter Link" class="icon icon--twitter"></a>
-	</section>
+<section class="wrapper cta social--strip center">
+	<h1 class="left">Don't forget to check us out on these social networks and tell your friends about your new gadgets.</h1>
+	<a href="http://www.facebook.com/sellmygadgets" title="Facebook Link" class="icon icon--facebook"></a>
+	<a href="#" title="Google Plus Link" class="icon icon--google-plus"></a>
+	<a href="http://www.twitter.com/sellmygadgets" title="Twitter Link" class="icon icon--twitter"></a>
+</section>
