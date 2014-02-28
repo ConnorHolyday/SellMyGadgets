20403c86825314e419700d1617f6c0c2e48dd878
Connor Holyday <connorholyday@gmail.com>
2014-02-28 12:04:55 +0000

    Started the product single view

--- application/view/Product/view.php
+++ application/view/Product/view.php
@@ -1,15 +1,70 @@
-<h1>Products Page </h1>
-
 <?php
-	echo 'Product id = ' . $this->productId . '<br>';
-	echo 'Name = ' . $this->productName . '<br>';
-	echo 'Price = ' . $this->productPrice . '<br>';
-	echo '<img src="' . $this->productImage . '.jpg" alt="' . $this->productImageName . '"><br>';
-	echo 'Image name = ' . $this->productImageName . '<br>';
-	//echo 'Category = ' . $this->productCatagory . '<br>';
-	echo 'Delivery date = ' . $this->productDeliveryDate . '<br>';
-	echo 'Delivery cost = ' . $this->productDeliveryCost . '<br>';
-	echo 'Condition = ' . $this->productCondition . '<br>';
-	echo 'Seller = ' . $this->productSeller . '<br>';	
-	echo 'Description = ' . $this->productDescription . '<br>';	
+	$productID = $this->productId;
+	$productName = $this->productName;
+	$productPrice = $this->productPrice;
+	$productImage = $this->productImage . '.jpg';
+	$productImageName = $this->productImageName;
+	//$productName 'Category = ' . $this->productCatagory;
+	$productDeliveryDate = $this->productDeliveryDate;
+	$productDeliveryCost = $this->productDeliveryCost;
+	$productCondition = $this->productCondition;
+	$productSeller = $this->productSeller;
+	//$productSellerID = $this->productSeller;
+	$productDescription = $this->productDescription;
 ?>
+
+<div class="wrapper">
+
+	<nav class="breadcrumb breadcrumb__product">
+		<a href="/">Home</a>
+		<a href="#">Product Search</a>
+		<a href="#"><?php echo $productName; ?></a>
+	</nav>
+
+	<div class="row">
+
+		<aside class="col d-2 sidebar__product">
+
+			<div class="boxed">
+				<img src="<?php echo $productImage ?>" alt="<?php $productImageName ?>">
+
+				<div class="slider">
+
+				</div>
+
+				<ul class="product-meta list-block__list">
+					<li><?php echo $productName ?></li>
+					<li>£<?php echo $productPrice ?></li>
+					<li>Condition - <?php echo $productCondition ?></li>
+					<li>Sold by: <a href="#"><?php echo $productSeller ?></a></li>
+				</ul>
+
+				<span class="tags">Tags: iPhone, Apple, Mobile, Phone</span>
+
+				<a href="#" class="btn btn--dark inline-block">Buy Product</a>
+			</div>
+
+		</aside>
+
+		<section class="col d-6">
+
+			<h1><?php echo $productName; ?></h1>
+			<span>Sold by: <a href="#"><?php echo $productSeller; ?></a></span>
+
+			<p><?php echo $productDescription; ?></p>
+
+			<div class="comments">
+				<h2>Comments</h2>
+			</div>
+
+		</section>
+
+	</div>
+
+	<section class="related-products">
+
+		<h1>Related Products</h1>
+
+	</section>
+
+</div>
