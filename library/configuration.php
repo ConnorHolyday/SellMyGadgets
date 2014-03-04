66cee6cabcdcbc7aeb75d64270c08b11b4c496a7
Ryan Trump <ryan.trump@live.co.uk>
2014-03-04 13:15:33 +0000

    Added config file

---
+++ library/configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Directories
+
+    define('APP_DIR', 'application/');
+    define('LIB_DIR', 'library/');
+    define('ASSET_DIR', '/assets/');
+
+    // Database connection parameters
+
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'sellmygadgets');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'sellmygadgets');
+
+    //pagination
+    define('PAGE_ITEMS', 9);
