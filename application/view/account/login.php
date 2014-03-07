f1a333d461b8d1764c9e2cbb6083517564863d49
Connor Holyday <connorholyday@gmail.com>
2014-03-07 16:14:31 +0000

    Login form styles

--- application/view/account/login.php
+++ application/view/account/login.php
@@ -1,17 +1,17 @@
   <section class="form form--login">
-    <form action="/account/login" method="post">
-      <h1>
-        Please Login to continue
-      </h1>
 
-      <div class="row">
-        <div class="col m-all t-2 d-4">
-          <label for="username" class="form__label">Username:</label>
-          <input type="text" name="username" placeholder="example@example.com" class="form__textbox">
+  <h1>Please Login to continue</h1>
+
+    <form action="/account/login" method="post">
+
+      <div class="row form__block">
+        <div class="col m-all t-2 d-4 / label__bottom">
+          <input type="text" name="username" placeholder="example@example.com" required>
+          <label for="username">Username:</label>
         </div>
-        <div class="col m-all t-2 d-4">
-          <label for="password" class="form__label">Password:</label>
-          <input type="password" name="password" placeholder="••••••••••" class="form__textbox">
+        <div class="col m-all t-2 d-4 / label__bottom">
+          <input type="password" name="password" placeholder="••••••••••" required>
+          <label for="password">Password:</label>
         </div>
       </div>
 
@@ -23,7 +23,7 @@
 
       <a href="/account/forgotten-password">Forgot your password?</a>
 
-      <button class="btn btn--dark pull-right">Login</button>
+      <button type="sbumit" class="btn btn--dark pull-right">Login</button>
 
     </form>
 
