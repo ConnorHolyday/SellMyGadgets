c459d345dc6452d78e25289d2e8878f24e36445a
Connor Holyday <connorholyday@gmail.com>
2014-03-08 20:57:47 +0000

    Added drag n drop animation

--- application/view/sell/images.php
+++ application/view/sell/images.php
@@ -41,7 +41,10 @@
   ;(function (w, d, undefined) {
 
     var dropZone = d.querySelector('.drop-zone'),
-      progressBar = d.querySelector('.file-progress');
+      progressBar = d.querySelector('.file-progress'),
+      anim = d.querySelector('.anim'),
+      anim2 = d.querySelector('.anim2'),
+      anim3 = d.querySelector('.anim3');
 
     dropZone.addEventListener('drop', function(e) {
       e.preventDefault();
@@ -55,14 +58,63 @@
 
     }, false);
 
-    dropZone.addEventListener('dragenter', function(e) {
+    dropZone.addEventListener('dragover', function(e) {
       e.preventDefault();
       e.stopPropagation();
     }, false);
 
-    dropZone.addEventListener('dragover', function(e) {
+    dropZone.addEventListener('dragenter', function(e) {
       e.preventDefault();
       e.stopPropagation();
+
+      if ( anim.classList.contains('__anim') && anim2.classList.contains('__anim2') && anim3.classList.contains('__anim3') ) {
+        anim.classList.remove('__anim');
+        anim.classList.add('_anim');
+        anim2.classList.remove('__anim2');
+        anim2.classList.add('_anim2');
+        anim3.classList.remove('__anim3');
+        anim3.classList.add('_anim3');
+      } else {
+        anim.classList.add('_anim');
+        anim2.classList.add('_anim2');
+        anim3.classList.add('_anim3');
+      }
+    }, false);
+
+    dropZone.addEventListener('dragleave', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if ( anim.classList.contains('_anim') && anim2.classList.contains('_anim2') && anim3.classList.contains('_anim3') ) {
+        anim.classList.remove('_anim');
+        anim.classList.add('__anim');
+        anim2.classList.remove('_anim2');
+        anim2.classList.add('__anim2');
+        anim3.classList.remove('_anim3');
+        anim3.classList.add('__anim3');
+      } else {
+        anim.classList.add('__anim');
+        anim2.classList.add('__anim2');
+        anim3.classList.add('__anim3');
+      }
+    }, false);
+
+    dropZone.addEventListener('drop', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if ( anim.classList.contains('_anim') && anim2.classList.contains('_anim2') && anim3.classList.contains('_anim3') ) {
+        anim.classList.remove('_anim');
+        anim.classList.add('__anim');
+        anim2.classList.remove('_anim2');
+        anim2.classList.add('__anim2');
+        anim3.classList.remove('_anim3');
+        anim3.classList.add('__anim3');
+      } else {
+        anim.classList.add('__anim');
+        anim2.classList.add('__anim2');
+        anim3.classList.add('__anim3');
+      }
     }, false);
 
     function sendFile(file) {
