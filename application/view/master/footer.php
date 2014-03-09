9b215e6da8b58de082c07634ae4157da735c4987
Ryan Trump <ryan.trump@live.co.uk>
2014-03-09 14:47:51 +0000

    Added optional js loader to base view / master

--- application/view/master/footer.php
+++ application/view/master/footer.php
@@ -24,4 +24,4 @@
     </footer>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="bower_components/jquery/jquery.js"><\/script>')</script>
-    <script src="/assets/js/main.js"></script>
+    <script src="/<?php echo STATIC_1; ?>js/main.js"></script>
--- library/configuration.php
+++ library/configuration.php
@@ -4,10 +4,9 @@
 
     define('APP_DIR', 'application/');
     define('LIB_DIR', 'library/');
-    define('ASSET_DIR', '/assets/');
     define('TMP_DIR', 'media/temp/');
 
-    define('STATIC_1', ''); // static1.sellmygadgets.co.uk - Site static content (css, images, js)
+    define('STATIC_1', 'assets/'); // static1.sellmygadgets.co.uk - Site static content (css, images, js)
     define('STATIC_2', ''); // static2.sellmygadgets.co.uk - User generated content (product images)
 
     // Database connection parameters
