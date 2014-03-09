88463548a036e80ccedbae9682cdfc4bfad177ba
Ryan Trump <ryan.trump@live.co.uk>
2014-03-09 21:34:15 +0000

    Added productview.js

--- application/view/Product/all.php
+++ application/view/Product/all.php
@@ -209,37 +209,3 @@
 
   </div>
 </div>
-
-<script type="text/javascript">
-
-  //*
-
-  ;(function (w, d, undefined) {
-
-    var viewList = d.querySelector('.view--list'),
-        viewGrid = d.querySelector('.view--grid'),
-        display = d.querySelector('.display--grid');
-
-    function toggleDisplay() {
-      display.classList.toggle('display--list');
-      display.classList.toggle('display--grid');
-    }
-
-    viewList.addEventListener('click', function(e){
-      e.preventDefault();
-      if ( display.classList.contains('display--grid') )
-        toggleDisplay();
-    });
-
-    viewGrid.addEventListener('click', function(e){
-      e.preventDefault();
-      if ( display.classList.contains('display--list') )
-        toggleDisplay();
-    });
-
-  })(window, document);
-
-  //*/
-
-</script>
-
