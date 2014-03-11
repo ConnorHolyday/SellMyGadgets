24f25fb036134a7eaa776387467cd29c9027e553
Ryan Trump <ryan.trump@live.co.uk>
2014-03-11 16:49:13 +0000

    Renamed index to home

---
+++ application/controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+  class HomeController extends BaseController {
+
+    function __construct() {
+      parent::__construct();
+    }
+
+    function index() {
+      $this->view->render('home/index', 'Homepage', true, true, [['file', 'smg.js'], ['inline'], '(function(d){smg.include(\'/home/stats\',d.querySelector(\'.js-stats\'));smg.include(\'/home/star-seller\',d.querySelector(\'.js-star\'));})(document);']);
+    }
+
+    function star_seller() {
+      $this->view->render_include('home/starseller');
+    }
+
+    function stats() {
+      $this->view->render_include('home/stats');
+    }
+  }
