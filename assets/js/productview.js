eeffcd0b4dfb7d405775f9dd1fb4815a126c79c6
Ryan <ryan.trump@live.co.uk>
2014-03-14 18:37:28 +0000

    Updated scripts to use smg.js library

--- assets/js/productview.js
+++ assets/js/productview.js
@@ -1,25 +1,18 @@
 
-;(function (w, d, undefined) {
-
-  var viewList = d.querySelector('.view--list'),
-      viewGrid = d.querySelector('.view--grid'),
-      display = d.querySelector('.display--grid');
-
-  function toggleDisplay() {
-    display.classList.toggle('display--list');
-    display.classList.toggle('display--grid');
-  }
-
-  viewList.addEventListener('click', function(e){
+;(function (d) {
+  var vl = d.querySelector('.view--list'),
+      vg = d.querySelector('.view--grid'),
+      dp = d.querySelector('.display--grid'),
+      toggleDisplay = function() {
+        _.cssClass.toggle(dp, 'display--list');
+        _.cssClass.toggle(dp, 'display--grid');
+      };
+  _.addEvent(vl, 'click', function(e) {
     e.preventDefault();
-    if ( display.classList.contains('display--grid') )
-      toggleDisplay();
+    toggleDisplay();
   });
-
-  viewGrid.addEventListener('click', function(e){
+  _.addEvent(vg, 'click', function(e) {
     e.preventDefault();
-    if ( display.classList.contains('display--list') )
-      toggleDisplay();
+    toggleDisplay();
   });
-
-})(window, document);
+})(document);
--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,29 +1,25 @@
   ;(function (w, d, undefined) {
 
-    var dropZone = d.querySelector('.drop-zone'),
+    var dz = d.querySelector('.drop-zone'),
       progressBar = d.querySelector('.file-progress'),
       anim = d.querySelector('.anim'),
       anim2 = d.querySelector('.anim2'),
       anim3 = d.querySelector('.anim3');
 
-    dropZone.addEventListener('drop', function(e) {
+    _.addEvent(dz, 'drop', function(e) {
       e.preventDefault();
       e.stopPropagation();
+      _.upload(e.dataTransfer.files, '/sell/upload');
 
-      var files = e.dataTransfer.files,
-        len = files.length > 5 ? 5 : files.length;
+      a1();
+    });
 
-      for(var i = 0; i < len; i++) {
-        sendFile(files[i]);
-      }
-    }, false);
-
-    dropZone.addEventListener('dragover', function(e) {
+    _.addEvent(dz, 'dragover', function(e) {
       e.preventDefault();
       e.stopPropagation();
-    }, false);
+    });
 
-    dropZone.addEventListener('dragenter', function(e) {
+    _.addEvent(dz, 'dragenter', function(e) {
       e.preventDefault();
       e.stopPropagation();
 
@@ -39,12 +35,16 @@
         anim2.classList.add('_anim2');
         anim3.classList.add('_anim3');
       }
-    }, false);
+    });
 
-    dropZone.addEventListener('dragleave', function(e) {
+    _.addEvent(dz, 'dragleave', function(e) {
       e.preventDefault();
       e.stopPropagation();
 
+      a1();
+    });
+
+    function a1() {
       if ( anim.classList.contains('_anim') && anim2.classList.contains('_anim2') && anim3.classList.contains('_anim3') ) {
         anim.classList.remove('_anim');
         anim.classList.add('__anim');
@@ -57,47 +57,6 @@
         anim2.classList.add('__anim2');
         anim3.classList.add('__anim3');
       }
-    }, false);
-
-    dropZone.addEventListener('drop', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-
-      if ( anim.classList.contains('_anim') && anim2.classList.contains('_anim2') && anim3.classList.contains('_anim3') ) {
-        anim.classList.remove('_anim');
-        anim.classList.add('__anim');
-        anim2.classList.remove('_anim2');
-        anim2.classList.add('__anim2');
-        anim3.classList.remove('_anim3');
-        anim3.classList.add('__anim3');
-      } else {
-        anim.classList.add('__anim');
-        anim2.classList.add('__anim2');
-        anim3.classList.add('__anim3');
-      }
-    }, false);
-
-    function sendFile(file) {
-
-      var xhr = new XMLHttpRequest(),
-        fd = new FormData();
-
-      xhr.open('POST', '/sell/upload', true);
-      fd.append('uploads[]', file);
-      fd.append('isAsync', 'true');
-      xhr.send(fd);
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status >= 200 && xhr.status < 400){
-          // Success
-          //console.log('Success');
-          console.log(xhr.responseText);
-        } else {
-          // server returned an error.
-          //console.log('Error');
-          console.log(xhr.responseText);
-        }
-      }, false);
     }
 
   })(window, document);
