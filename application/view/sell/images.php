f89ee715987178cb00b39d4f9b4f8334d845c39c
Connor Holyday <connorholyday@gmail.com>
2014-03-18 21:11:31 +0000

    Finished image upload styling

--- application/view/sell/images.php
+++ application/view/sell/images.php
@@ -4,34 +4,54 @@
       <h1>Sell an item - Upload Images</h1>
     </div>
   </div>
+
   <div class="wrapper">
-    <nav itemscope itemtype ="https://schema.org/breadcrumb" class="breadcrumb breadcrumb__sell">
+
+    <nav itemscope itemtype="https://schema.org/breadcrumb" class="breadcrumb breadcrumb__sell">
       <a itemprop="url" href="/sell/item/details">Details</a>
       <a itemprop="url" href="/sell/item/images" class="selected">Images</a>
       <a itemprop="url" href="/sell/item/delivery">Delivery</a>
       <a itemprop="url" href="/sell/item/confirm">Review</a>
     </nav>
+
     <form action="/sell/item/delivery" method="post" class="submit-form" enctype="multipart/form-data">
+
       <div class="drop-zone__wrapper">
+
         <h1>Drag files here to upload</h1>
 
         <div class="drop-zone__wrapper-inner">
+
           <div class="drop-zone vertical-align">
+
             <span class="arrow">&#8593;</span>
+
             <span class="anim"></span>
+
             <span class="anim2"></span>
+
             <span class="anim3"></span>
+
           </div>
+
         </div>
+
       </div>
-      OR
-      <p>
-        Use the upload control below
-      </p>
+
+      <span class="sell-divider">or</span>
+
+      <p class="sell-divider__p">Use the upload control below.</p>
+
       <div class="form__block">
+
         <input type="file" name="uploads[]" multiple>
+
       </div>
+
       <button class="form__submit pull-right">Next Stage</button>
+
     </form>
+
   </div>
+
 </section>
