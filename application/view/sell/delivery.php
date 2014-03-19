c0bade3e63619edfd72e7e48191bda6d115cc11b
Connor Holyday <connorholyday@gmail.com>
2014-03-19 21:21:02 +0000

    Sell process fully responsive

--- application/view/sell/delivery.php
+++ application/view/sell/delivery.php
@@ -17,27 +17,28 @@
 
     <form action="/sell/item/confirm" method="post" class="form submit-form">
 
-      <div class="row form__block">
-        <div class="col m-1 t-3 d-4">
+      <div class="row">
+        <div class="col m-1 t-3 d-4 / form__block">
           <input type="radio" name="del_type" value="1" id="r1" required>
           <label for="r1">Delivery</label>
         </div>
-        <div class="col m-1 t-3 d-4">
+        <div class="col m-1 t-3 d-4 / form__block">
           <input type="radio" name="del_type" value="2" id="r2" required>
           <label for="r2">Collection</label>
         </div>
       </div>
 
-      <div class="row / form__block">
-        <div class="col m-all t-3 d-4 / label__bottom">
+      <div class="row">
+        <div class="col m-all t-3 d-4 / form__block label__bottom">
           <input type="number" min="0" name="del_price" id="del_price" required>
           <label for="del_price">Delivery Price (£)</label>
         </div>
-        <div class="col m-all t-3 d-4 / label__bottom">
+        <div class="col m-all t-3 d-4 / form__block label__bottom">
           <input type="text" min="0" name="del_details" id="del_details" required>
           <label for="del_details">Delivery Details - E.g. First Class</label>
         </div>
       </div>
+      
       <div class="form__block / label__bottom">
         <textarea name="coll_details" id="coll_details" required></textarea>
         <label for="coll_details">Collection Details</label>
--- application/view/sell/details.php
+++ application/view/sell/details.php
@@ -22,13 +22,13 @@
         <label for="productname">Product Name</label>
       </div>
 
-      <div class="row / form__block">
-        <div class="col m-all t-2 d-2 / label__bottom">
+      <div class="row">
+        <div class="col m-all t-2 d-2 / form__block label__bottom">
           <input type="number" min="0" name="price" required>
           <label for="price">Price (£)</label>
         </div>
 
-        <div class="col m-all t-2 d-2 / select__block">
+        <div class="col m-all t-2 d-2 / form__block select__block">
           <label for="condition">Condition:</label>
           <select name="condition">
             <?php echo $this->conditions; ?>
@@ -36,15 +36,15 @@
         </div>
       </div>
 
-      <div class="row / form__block">
-        <div class="col m-all t-2 d-2 / select__block">
+      <div class="row">
+        <div class="col m-all t-2 d-2 / form__block select__block">
           <label for="category">Category:</label>
           <select name="category">
             <?php echo $this->categories; ?>
           </select>
         </div>
 
-        <div class="col m-all t-2 d-2 / select__block">
+        <div class="col m-all t-2 d-2 / form__block select__block">
           <label for="brand">Brand:</label>
           <select name="brand">
             <?php echo $this->brands; ?>
